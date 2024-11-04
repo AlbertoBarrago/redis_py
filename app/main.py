b5f03fd0d93c359b02eb73f011a4bc307a97ec32
alBz <abarrago@minsait.com>
2024-11-04 15:07:21 +0100

    Implement RESP protocol handling in client requests
    Refactored `parse_request` to encode client data in RESP format. Updated `handle_client` to parse and respond to "ping" and "echo" commands according to the RESP specification. This enhances the server's ability to process Redis-like commands effectively.

--- app/main.py
+++ app/main.py
@@ -1,21 +1,24 @@
 """ Redi from scratch -> https://app.codecrafters.io/ """
 import socket  # noqa: F401
 import threading  # noqa: F401
-from urllib.parse import urlparse, parse_qs  # noqa: F401
 
 
-def parse_request(request: str):
-    lines = request.split()
+def parse_request(data, encoding="utf-8"):
+    if not isinstance(data, list):
+        data = [data]
 
-    print(f"Lines: {lines}")
-    command = lines[1].upper()
-    args = None
-    if command not in ['PING']:
-        args = lines[2]
+    separator = "\r\n"
+    size = len(data)
+    encoded = []
 
-    print(f"Command: {command}, args: {args}")
+    for args in data:
+        encoded.append(f"${len(args)}")
+        encoded.append(args)
+    if size > 1:
+        encoded.insert(0, f"*{size}")
 
-    return command, args
+    print(f"encoded: {encoded}")
+    return (separator.join(encoded) + separator).encode(encoding=encoding)
 
 
 def handle_client(client_socket):
@@ -24,27 +27,28 @@ def handle_client(client_socket):
     :param client_socket:
     :return:
     """
-    try:
-        while True:
-            request = client_socket.recv(1024).decode("utf-8")
+    while True:
+        data = client_socket.recv(32)
 
-            if not request:
-                break
-            # print(f"Received request: {request}")
+        if not data:
+            break
 
-            command, args = parse_request(request)
-            print(f"Received request: {command}, args: {args}")
+        print("Received {!r}".format(data))
+        arr_size, *arr = data.split(b"\r\n")
+        print(f"Array size: {arr_size}")
+        print(f"Array content: {arr}")
 
-            response = "-Error: Unsupported command\r\n"
-            if command == 'ECHO':
-                response = f"${len(args)}\r\n{args}\r\n"
-            elif command == 'PING':
-                response = "+PONG\r\n"
-            client_socket.sendall(response.encode("utf-8"))
-            print(f"Sent response: {response}")
-    except ConnectionResetError:
-        print("Client disconnected")
-    finally:
+        if arr[1] == b"ping":
+            resp = parse_request("PONG")
+            print(f"Sending PONG reps ->, {resp}")
+            print(f"Response sent {resp}")
+            client_socket.sendAll(resp)
+        elif arr[1] == b"echo":
+            resp = parse_request([el.decode("utf-8") for el in arr[3::2]])
+            print(f"Response sent {resp}")
+            client_socket.sendAll(resp)
+        else:
+            break
         client_socket.close()
 
 
@@ -59,8 +63,7 @@ def main():
         while True:
             client_socket, addr = server_socket.accept()
             print(f"Connected to {addr}")
-            client_thread = threading.Thread(target=handle_client, args=(client_socket,))
-            client_thread.start()
+            threading.Thread(target=handle_client, args=(client_socket,)).start()
     except KeyboardInterrupt:
         print("Shutting down server")
     finally:
