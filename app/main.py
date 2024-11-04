fd548a31c14b9c9fa8b9b877e2317113cab9b5da
alBz <abarrago@minsait.com>
2024-11-04 11:35:35 +0100

    Setup initial project files and basic server implementation
    Added essential project configuration files for IntelliJ IDEA. Implemented basic server functionality in `app/main.py` to accept a client, handle a simple request, and send a response.

--- app/main.py
+++ app/main.py
@@ -7,7 +7,20 @@ def main():
 
     # Uncomment this to pass the first stage
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    try:
+        client, addr = server_socket.accept() # wait for client
+        print(f"Connected to {addr}")
+        request = client.recv(1024).decode("utf-8")
+        print(f"Received request {request}")
+        response = "+PONG\r\n"
+        client.sendall(response.encode("utf-8"))
+        print(f"Sent response {response}")
+        client.close()
+    except KeyboardInterrupt:
+        print("Shutting down server")
+    finally:
+        server_socket.close()
+
 
 
 if __name__ == "__main__":
